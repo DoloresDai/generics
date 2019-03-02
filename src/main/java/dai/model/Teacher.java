6348655f4c10f7550efd15bd1059a9fa16ecc294
DoloresDai <841856753@qq.com>
2019-03-02 18:19:43 +0800

    Create a teacher class that inherits Person, add the subject attribute.

---
+++ src/main/java/dai/model/Teacher.java
@@ -0,0 +1,18 @@
+package main.java.dai.model;
+
+public class Teacher extends Person {
+    private String subject;
+
+    public Teacher(String name, int age, String subject) {
+        super(name, age);
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
